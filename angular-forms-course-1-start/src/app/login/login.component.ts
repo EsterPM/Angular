caf1fa4e4ff6d1c3e2ce65b93a444b8670006b51
Esther Pont <esther.cescit@gmail.com>
2025-04-23 16:34:11 +0200

    ngModel: Options i change

--- angular-forms-course-1-start/src/app/login/login.component.ts
+++ angular-forms-course-1-start/src/app/login/login.component.ts
@@ -10,7 +10,6 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-
   constructor() {
 
 
@@ -22,6 +21,10 @@ export class LoginComponent implements OnInit {
 
   login(loginForm: NgForm, submit) {
     console.log(loginForm.value, loginForm.valid, submit);
+  }
+
+  onEmailChange(change) {
+    console.log(change);
   }
 
 }
