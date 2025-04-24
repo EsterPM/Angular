658f128b6e9cca7e463c7c078044b43c67978067
Esther Pont <esther.cescit@gmail.com>
2025-04-24 17:22:19 +0200

    Formulari de creació: Dropdpwn amb opcions des del backend

--- angular-forms-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-forms-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -5,6 +5,11 @@ import { Observable } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { courseTitleValidator } from '../../validators/course-title.validator';
 
+interface CourseCategory {
+  code:string;
+  description:string;
+}
+
 @Component({
   selector: 'create-course-step-1',
   templateUrl: './create-course-step-1.component.html',
@@ -24,16 +29,21 @@ export class CreateCourseStep1Component implements OnInit {
       updateOn: 'blur'
     }],
     releasedAt: [new Date(), Validators.required], //Un valor inicial que és la data actual
+    category: ['BEGINNER', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue], //El valor inicial és false (la casella de verificació està desmarcada per defecte)
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  //Obtenció de les categories del curs
+  courseCategories$ : Observable<CourseCategory[]>;
 
   constructor(private fb: FormBuilder, private courses: CoursesService) {
 
   }
 
   ngOnInit() {
-
+    // retorna un array de categories per mostrar-lo al formulari
+    this.courseCategories$ = this.courses.findCourseCategories();
   }
 
   //facilita accedir al control des del HTML.
