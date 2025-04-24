b21429e0c7a5465a7e88f5e95d1476f4a0893218
Esther Pont <esther.cescit@gmail.com>
2025-04-24 18:06:50 +0200

    Formulari de creació: Habilitar i desabilitar camps del form

--- angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,17 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
-    selector: 'create-course-step-2',
-    templateUrl: 'create-course-step-2.component.html',
-    styleUrls: ['create-course-step-2.component.scss'],
-    standalone: false
+  selector: 'create-course-step-2',
+  templateUrl: 'create-course-step-2.component.html',
+  styleUrls: ['create-course-step-2.component.scss'],
+  standalone: false
 })
 export class CreateCourseStep2Component implements OnInit {
 
   form = this.fb.group({
-    courseType: ['premium', Validators.required]
+    courseType: ['premium', Validators.required],
+    price: [null, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern("[0-9]+")
+    ]],
   });
 
   constructor(private fb: FormBuilder) {
@@ -19,9 +25,20 @@ export class CreateCourseStep2Component implements OnInit {
   }
 
   ngOnInit() {
+    //valueChanges és un observable que emet un valor cada vegada que canvia alguna dada dins del formulari.
+    this.form.valueChanges
+      .subscribe(val => {
+        const priceControl = this.form.controls["price"];
 
+        //Si el curs és gratuït, deshabilitem el camp de preu
+        if (val.courseType == 'free' && priceControl.enabled) {
+          priceControl.disable({ emitEvent: false }); //{ emitEvent: false } s'utilitza per evitar que el canvi en l'estat del control (disabled) emeti un nou esdeveniment que podria desencadenar canvis addicionals, com les validacions de formulari.
+        }
+        //Si el curs és premium, habilitem el camp de preu
+        else if (val.courseType == 'premium' && priceControl.disabled) {
+          priceControl.enable({ emitEvent: false });
+        }
 
-
+      });
   }
-
 }
