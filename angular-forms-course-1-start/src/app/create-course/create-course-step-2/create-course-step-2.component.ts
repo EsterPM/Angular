36948722e8731a25ca6d7adf2af6a9b156d9b434
Esther Pont <esther.cescit@gmail.com>
2025-04-24 17:39:09 +0200

    Formulari de creació: Errors al cambiar de pas

--- angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ angular-forms-course-1-start/src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -10,6 +10,13 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class CreateCourseStep2Component implements OnInit {
 
+  form = this.fb.group({
+    courseType: ['premium', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) {
+
+  }
 
   ngOnInit() {
 
--- angular-forms-course-1-start/src/app/create-course/create-course.component.ts
+++ angular-forms-course-1-start/src/app/create-course/create-course.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 
 
 
 @Component({
-    selector: 'create-course',
-    templateUrl: './create-course.component.html',
-    styleUrls: ['./create-course.component.scss'],
-    standalone: false
+  selector: 'create-course',
+  templateUrl: './create-course.component.html',
+  styleUrls: ['./create-course.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true } //Configura que els errors es mostrin al canviar de pas
+    }
+  ],
+  standalone: false
 })
 export class CreateCourseComponent implements OnInit {
 
