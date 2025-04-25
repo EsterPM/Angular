f53ac7eaca5d4bb3c7c8bd6a5035b21a4c4a783b
Esther Pont <esther.cescit@gmail.com>
2025-04-25 10:33:48 +0200

    File upload: estructura

--- angular-forms-course-1-start/src/app/file-upload/file-upload.component.ts
+++ angular-forms-course-1-start/src/app/file-upload/file-upload.component.ts
@@ -1,17 +1,30 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HttpClient, HttpEventType } from '@angular/common/http';
-import {catchError, finalize} from 'rxjs/operators';
-import {AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
-import {noop, of} from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
+import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator } from '@angular/forms';
+import { noop, of } from 'rxjs';
 
 
 @Component({
-    selector: 'file-upload',
-    templateUrl: "file-upload.component.html",
-    styleUrls: ["file-upload.component.scss"],
-    standalone: false
+  selector: 'file-upload',
+  templateUrl: "file-upload.component.html",
+  styleUrls: ["file-upload.component.scss"],
+  standalone: false
 })
-export class FileUploadComponent {
 
+export class FileUploadComponent {
+  @Input()
+  requiredFileType: string;
+
+  fileName = '';
+
+
+  onFileSelected(event) {
+    //el primer fitxer seleccionat
+    const file: File = event.target.files[0];
+    //Assigna el nom del fitxer a la variable fileName
+    this.fileName = file.name;
+    console.log(this.fileName);
+  }
 
 }
