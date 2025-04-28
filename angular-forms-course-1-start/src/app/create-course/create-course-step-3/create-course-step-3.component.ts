37967bc8e801debf22cc085d7ac5cdd8bad2964f
Esther Pont <esther.cescit@gmail.com>
2025-04-28 10:28:58 +0200

    Form Arrays

--- angular-forms-course-1-start/src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ angular-forms-course-1-start/src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,20 +1,39 @@
-import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
-    selector: 'create-course-step-3',
-    templateUrl: 'create-course-step-3.component.html',
-    styleUrls: ['create-course-step-3.component.scss'],
-    standalone: false
+  selector: 'create-course-step-3',
+  templateUrl: 'create-course-step-3.component.html',
+  styleUrls: ['create-course-step-3.component.scss'],
+  standalone: false
 })
 export class CreateCourseStep3Component {
 
   form = this.fb.group({
-
+    lessons: this.fb.array([])
   });
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb: FormBuilder) {
 
+  }
+
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
+  //Es crea un nou FormGroup i s'afegeix dins de l'Array
+  addLesson() {
+    const lessonForm = this.fb.group({
+      title: ['', Validators.required],
+      level: ['beginner', Validators.required]
+    });
+
+    this.lessons.push(lessonForm);
+  }
+
+  //Elimina la lliçó en la posició i
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
   }
 }
