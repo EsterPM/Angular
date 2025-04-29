c85ef010f23d60664d6a2d535a75c770ed2a2d27
Esther Pont <esther.cescit@gmail.com>
2025-04-29 12:01:08 +0200

    Afegir Lazy loading

--- angular-router-course-1-start/src/app/app-routing.module.ts
+++ angular-router-course-1-start/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules, UrlSerializer } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
+  //Lazy loading és una tècnica que carrega els mòduls només quan són necessaris, en lloc de carregar-los tots al principi.
+  //Millora el rendiment inicial de l'aplicació.
+  //Angular només carregarà CoursesModule quan l'usuari accedeixi a /courses
+  {
+    path: "courses",
+    loadChildren: () => import('./courses/courses.module')
+      .then(m => m.CoursesModule)
+  },
   {
     path: "login",
     component: LoginComponent
--- angular-router-course-1-start/src/app/courses/courses-routing.module.ts
+++ angular-router-course-1-start/src/app/courses/courses-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+//Perquè funcioni lazy loading:
+//El CoursesModule ha de tenir el seu Routing Module (CoursesRoutingModule) amb rutes pròpies.
 
 const routes: Routes = [
-
+  {
+    path: "",
+    component: HomeComponent
+  }
 ];
 
 @NgModule({
+  //S'ha d'usar RouterModule.forChild() (no forRoot()).
   imports: [
-
+    RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
   providers: [
