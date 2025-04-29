272661c564475e93f41d0fd6c424d0491921adef
Esther Pont <esther.cescit@gmail.com>
2025-04-29 18:37:13 +0200

    Guards: introduction

--- angular-router-course-1-start/src/app/login/login.component.ts
+++ angular-router-course-1-start/src/app/login/login.component.ts
@@ -1,22 +1,22 @@
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
 import { LoadingService } from './../shared/loading/loading.service';
-import {Component, Input, OnInit} from '@angular/core';
-import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 
-import {AuthStore} from '../services/auth.store';
+import { AuthStore } from '../services/auth.store';
 
 //activar un indicador de càrrega (loading) mentre es carrega o canvia una ruta.
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: false
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: false
 })
 export class LoginComponent implements OnInit {
 
-  //form: UntypedFormGroup;
+  form: FormGroup;
 
   @Input()
   routing: boolean = false;
@@ -25,52 +25,37 @@ export class LoginComponent implements OnInit {
   detectRoutingOngoing = false;
 
   constructor(
-    public loadingService: LoadingService,
-    //private fb: UntypedFormBuilder,
+    private fb: FormBuilder,
     private router: Router,
-    //private auth: AuthStore
-    ) {
+    private auth: AuthStore
+  ) {
 
-    /*this.form = fb.group({
+    this.form = fb.group({
       email: ['test@angular-university.io', [Validators.required]],
       password: ['test', [Validators.required]]
-    });*/
+    });
 
   }
 
   ngOnInit() {
-    //Escolta els esdeveniments del Router, però només si detectRoutingOngoing === true.
-    if (this.detectRoutingOngoing) {
-      this.router.events
-        .subscribe(
-          event => {
-            //Activa el loading quan: comença la navegacio o es comença a carregar un mòdul amb lazy loading (RouteConfigLoadStart)
-            if (event instanceof NavigationStart || event instanceof RouteConfigLoadStart) {
-              this.loadingService.loadingOn();
-            }
-            //Desactiva el loading
-            else if (event instanceof NavigationEnd ||
-                event instanceof NavigationError ||
-                event instanceof NavigationCancel ||
-                event instanceof RouteConfigLoadEnd) {
-                  this.loadingService.loadingOff();
-                }
-          }
-        )
-    }
+
   }
 
-  /*login() {
+  login() {
 
+    //Obtiene los valores del formulario
     const val = this.form.value;
 
+    //Llama al servicio de autenticación:
     this.auth.login(val.email, val.password)
-        .subscribe(
-            () => {},
-            err => {
-                alert("Login failed!");
-            }
-        );
-  }*/
-
+      .subscribe(
+        //En caso de éxito, redirige al usuario a /courses
+        () => {
+          this.router.navigateByUrl('/courses')
+        },
+        err => {
+          alert("Login failed!");
+        }
+      );
+  }
 }
