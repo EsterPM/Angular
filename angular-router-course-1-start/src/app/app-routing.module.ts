e9837ac28916a13eeb6d59c6a18bc5ec48c128f8
Esther Pont <esther.cescit@gmail.com>
2025-04-29 12:27:06 +0200

    Configurar ruta base i not found

--- angular-router-course-1-start/src/app/app-routing.module.ts
+++ angular-router-course-1-start/src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules, UrlSerializer } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
+  //redirecciona automàticament l'usuari a /courses quan visita el domini base
+  {
+    path: "",
+    redirectTo: "/courses",
+    pathMatch: "full" //Fa que la coincidència es faci només si la ruta és exactament buida. (Sense això, podria intentar redireccionar també subrutes.)
+  },
+
   //Lazy loading és una tècnica que carrega els mòduls només quan són necessaris, en lloc de carregar-los tots al principi.
   //Millora el rendiment inicial de l'aplicació.
   //Angular només carregarà CoursesModule quan l'usuari accedeixi a /courses
@@ -20,8 +28,14 @@ const routes: Routes = [
   {
     path: "about",
     component: AboutComponent
-  }
+  },
 
+  //Important posar al final
+  //captura qualsevol ruta que no coincideixi amb cap altra definida abans.
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
