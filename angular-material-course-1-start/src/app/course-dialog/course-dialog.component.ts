5ea6d02fd00e1e13af7c0f568a969f1451184306
Esther Pont <esther.cescit@gmail.com>
2025-05-04 13:26:35 +0200

    Dialogs

--- angular-material-course-1-start/src/app/course-dialog/course-dialog.component.ts
+++ angular-material-course-1-start/src/app/course-dialog/course-dialog.component.ts
@@ -1,25 +1,68 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA,  MatDialog,  MatDialogConfig,  MatDialogRef} from '@angular/material/dialog';
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css'],
-    standalone: false
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css'],
+  standalone: false
 })
 export class CourseDialogComponent implements OnInit {
 
-    constructor(private fb: FormBuilder) {
+  description: string;
 
+  //Es crea un formulari reactiu amb els valors per defecte del curs (this.course) injectat.
+  form = this.fb.group({
+    description: [this.course.description, Validators.required],
+    category: [this.course.category, Validators.required],
+    releasedAt: [new Date(), Validators.required],
+    longDescription: [this.course.longDescription, Validators.required]
+  });
 
-    }
+  constructor(private fb: FormBuilder, @Inject(MAT_DIALOG_DATA) private course: Course,
+    private dialogRef: MatDialogRef<CourseDialogComponent>) {
 
-    ngOnInit() {
+    //S'assigna la descripció del curs a una variable de suport
+    this.description = course.description;
+  }
 
-    }
+  ngOnInit() {
 
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  //Tanca el diàleg i retorna els valors del formulari.
+  save() {
+    this.dialogRef.close(this.form.value);
+  }
 }
 
+//obre el diàleg amb configuració específica
+export function openEditCourseDialog(dialog: MatDialog, course: Course) {
+
+  const config = new MatDialogConfig();
+
+  //evita que es tanqui fent clic fora
+  config.disableClose = true;
+  config.autoFocus = true;
+
+  //permeten personalitzar l'estil.
+  config.panelClass = "modal-panel";
+  config.backdropClass = "backdrop-modal-panel";
+
+  //Es passa el curs com a data.
+  config.data = {
+    ...course
+  };
+
+  //Retorna un observable de afterClosed(), que es pot subscriure per rebre els valors del formulari un cop tancat.
+  const dialogRef = dialog.open(CourseDialogComponent, config);
+
+  return dialogRef.afterClosed();
+}
--- angular-material-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
+++ angular-material-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
@@ -1,30 +1,41 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.css'],
-    standalone: false
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css'],
+  standalone: false
 })
 export class CoursesCardListComponent implements OnInit {
 
-    @Input()
-    courses: Course[];
+  @Input()
+  courses: Course[];
 
-    constructor() {
-    }
+  constructor(private dialog: MatDialog) {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-    }
+  }
 
-    editCourse(course:Course) {
+  //editar un curs obrint un diàleg, i després capturar els valors modificats si l'usuari fa clic a "Guardar"
+  editCourse(course: Course) {
 
-
-    }
-
+    //Obre el diàleg de formulari usant la funció
+    openEditCourseDialog(this.dialog, course)
+      .pipe(
+        //Filtra el resultat retornat
+        filter(val => !!val)
+      )
+      //Quan el diàleg es tanca i es retorna un objecte vàlid
+      .subscribe(
+        val => console.log("new course value:", val)
+      );
+  }
 }
 
 
