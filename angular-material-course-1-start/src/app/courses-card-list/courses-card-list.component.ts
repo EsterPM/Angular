5da1e9985533675b6d4edf6d4524390ed4b33224
Esther Pont <esther.cescit@gmail.com>
2025-05-05 13:58:14 +0200

    Responsive Design: Breakpoint (diferents dispositius)

--- angular-material-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
+++ angular-material-course-1-start/src/app/courses-card-list/courses-card-list.component.ts
@@ -3,6 +3,7 @@ import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'courses-card-list',
@@ -15,11 +16,52 @@ export class CoursesCardListComponent implements OnInit {
   @Input()
   courses: Course[];
 
-  constructor(private dialog: MatDialog) {
+  //Responsive
+  cols = 1;
+  rowHeight = '500px';
+  handsetPortrait = false; //booleà per saber si estem en mòbil vertical
+
+
+  //MatDialog per obrir diàlegs. //BreakpointObserver per detectar els canvis de mida/dispositiu.
+  constructor(private dialog: MatDialog, private responsive: BreakpointObserver) {
   }
 
-  ngOnInit() {
 
+  ngOnInit() {
+    //Escolta els breakpoints predeterminats de @angular/cdk/layout, que identifiquen dispositius i orientacions concretes.
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ])
+      .subscribe(result => {
+        //Valors per defecte
+        this.cols = 3;
+        this.rowHeight = "500px";
+        this.handsetPortrait = false;
+
+        const breakpoints = result.breakpoints;
+
+        //Tablet en vertical
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.cols = 1;
+        }
+        //Mòbil en vertical
+        else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.cols = 1;
+          this.rowHeight = "430px";
+          this.handsetPortrait = true;
+        }
+        //Mòbil en horitzontal
+        else if (breakpoints[Breakpoints.HandsetLandscape]) {
+          this.cols = 1;
+        }
+        //Tablet en horitzontal
+        else if (breakpoints[Breakpoints.TabletLandscape]) {
+          this.cols = 2;
+        }
+      });
   }
 
   //editar un curs obrint un diàleg, i després capturar els valors modificats si l'usuari fa clic a "Guardar"
