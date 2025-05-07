da81b43efda64c715b383d5032a54a3c49e1b110
Esther Pont <esther.cescit@gmail.com>
2025-05-07 17:00:43 +0200

    Authentication state management: Adaptar a login

--- reactive-angular-course-1-start/src/app/login/login.component.ts
+++ reactive-angular-course-1-start/src/app/login/login.component.ts
@@ -1,14 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthStore } from '../services/auth.store';
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    standalone: false
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: false
 })
 export class LoginComponent implements OnInit {
 
@@ -16,7 +17,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private router: Router) {
+    private router: Router,
+    private auth: AuthStore) {
 
     this.form = fb.group({
       email: ['test@angular-university.io', [Validators.required]],
@@ -30,11 +32,19 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
+    //S'obtenen els valors del formulari
     const val = this.form.value;
 
-
-
+    //Es crida al mètode login del servei d'autenticació
+    this.auth.login(val.email, val.password)
+      .subscribe(
+        () => {
+          //Si el login té èxit, es redirigeix a la pàgina de cursos
+          this.router.navigateByUrl("/courses")
+        },
+        err => {
+          alert("Login failed!");
+        }
+      );
   }
-
 }
