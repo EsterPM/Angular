544e13b7cadb7a944a7008b15842e65ac51f6401
Esther Pont <esther.cescit@gmail.com>
2025-05-09 15:48:23 +0200

    Bàsic de signals: Effects amb injector

--- angular-signals-course-1-start/src/app/home/home.component.ts
+++ angular-signals-course-1-start/src/app/home/home.component.ts
@@ -1,12 +1,12 @@
-import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
-import {CoursesService} from "../services/courses.service";
-import {Course, sortCoursesBySeqNo} from "../models/course.model";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MessagesService} from "../messages/messages.service";
-import {catchError, from, throwError} from "rxjs";
-import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+import { afterNextRender, Component, computed, effect, inject, Injector, signal } from '@angular/core';
+import { CoursesService } from "../services/courses.service";
+import { Course, sortCoursesBySeqNo } from "../models/course.model";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { CoursesCardListComponent } from "../courses-card-list/courses-card-list.component";
+import { MatDialog } from "@angular/material/dialog";
+import { MessagesService } from "../messages/messages.service";
+import { catchError, from, throwError } from "rxjs";
+import { toObservable, toSignal, outputToObservable, outputFromObservable } from "@angular/core/rxjs-interop";
 
 //Tipo del objecte
 type Counter = {
@@ -14,23 +14,34 @@ type Counter = {
 };
 
 @Component({
-    selector: 'home',
-    imports: [
-        MatTabGroup,
-        MatTab,
-        CoursesCardListComponent
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'home',
+  imports: [
+    MatTabGroup,
+    MatTab,
+    CoursesCardListComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
   counter = signal(0);
 
+  //sistema d'injecció de dependències d'Angular
+  injector = inject(Injector);
+
   constructor() {
-    //executa una funció cada vegada que algun signal utilitzat dins seu canvia.
-    effect(() => {
-      console.log(`counter value: ${this.counter()}`);
+
+    //Fa que el codi que hi ha dins s'executi després de la primera renderització del component.
+    afterNextRender(() => {
+      //Permet vincular l'effect al cicle de vida del component.
+      //Si el component es destrueix, l'efecte també s'elimina automàticament.
+      effect(() => {
+        console.log(`counter value: ${this.counter()}`);
+      },
+      {
+        injector:this.injector
+      })
     })
   }
 
@@ -41,4 +52,6 @@ export class HomeComponent {
 }
 
 
-//per: console.log, peticions HTTP, actualitzar el DOM fora d'Angular, etc.
+/*Sense injector, l'effect() no estaria vinculat al component,
+i podria quedar-se actiu fins i tot després que el component hagi estat destruït,
+generant fuites de memòria. Amb injector, Angular gestiona la subscripció per tu.*/
