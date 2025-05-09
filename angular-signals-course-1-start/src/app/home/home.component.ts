489ccf8372e5d2150a7e408876bb0c38640628dd
Esther Pont <esther.cescit@gmail.com>
2025-05-09 17:08:18 +0200

    CRUD: sense signals

--- angular-signals-course-1-start/src/app/home/home.component.ts
+++ angular-signals-course-1-start/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { CoursesServiceWithFetch } from './../services/courses-fetch.service';
 import { timeout } from 'rxjs/operators';
 import { afterNextRender, Component, computed, effect, EffectRef, inject, Injector, signal } from '@angular/core';
 import { CoursesService } from "../services/courses.service";
@@ -9,10 +10,6 @@ import { MessagesService } from "../messages/messages.service";
 import { catchError, from, throwError } from "rxjs";
 import { toObservable, toSignal, outputToObservable, outputFromObservable } from "@angular/core/rxjs-interop";
 
-//Tipo del objecte
-type Counter = {
-  value: number
-};
 
 @Component({
   selector: 'home',
@@ -25,42 +22,27 @@ type Counter = {
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  courses = signal<Course[]>([]);
 
-  counter = signal(0);
-
-  //referència a l'effect(), que et permet destruir-lo manualment més endavant.
-  effectRef: EffectRef | null = null;
-
+  coursesService = inject(CoursesServiceWithFetch);
 
   constructor() {
-
-    this.effectRef = effect((onCleanup) => {
-      const counter = this.counter();
-      //Espera 1 segon abans de fer un console.log
-      const timeout = setTimeout(() => {
-        console.log(`counter value: ${counter}`);
-      }, 1000)
-
-      //S'executa abans que aquest effect() es torni a executar de nou.
-      //També s'executa quan el effect() es destrueix amb .destroy().
-      onCleanup(() => {
-        console.log("Callin clean up");
-        clearTimeout(timeout);
-      })
-    })
+    this.loadCourses()
+      .then(() => console.log(`All courses loaded:`, this.courses()));
   }
 
-
-  //Incrementa el counter en 1.
-  increment() {
-    this.counter.update(val => val + 1);
+  async loadCourses() {
+    try {
+      const courses = await this.coursesService.loadAllCourses();
+      this.courses.set(courses);
+    }
+    catch (err) {
+      alert(`Error loading courses!`);
+      console.error(err);
+    }
   }
 
-  //Destrueix completament l'effect(), El counter() pot canviar, però l'effect() ja no reaccionarà.
-  cleanup() {
-    this.effectRef?.destroy();
-  }
 }
 
 
-//Això és per temporitzadors, intervals, subscripcions, etc.
+
