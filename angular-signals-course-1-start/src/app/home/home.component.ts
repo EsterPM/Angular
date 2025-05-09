5679aa2b3b68ae758df1c2a9da5e6a3db97cb001
Esther Pont <esther.cescit@gmail.com>
2025-05-09 12:06:40 +0200

    Bàsic de signals: Computed (recalcular auto)

--- angular-signals-course-1-start/src/app/home/home.component.ts
+++ angular-signals-course-1-start/src/app/home/home.component.ts
@@ -25,14 +25,28 @@ type Counter = {
 })
 export class HomeComponent {
 
-  //Array
-  values = signal<number[]>([0])
+  counter = signal(0);
 
-  //crea un nou array (important perquè els signals detectin el canvi).
-  append() {
-    this.values.update(values => ([
-      ...values,
-      values[values.length - 1] + 1 //agafa l'últim element i li suma 1.
-    ]));
+  //és un signal derivat de counter.
+  //Cada vegada que counter canvia, aquest computed es recalcula automàticament.
+  tenXCounter = computed(() => {
+    const val = this.counter();
+    return val * 10;
+  })
+
+  //computed, que depèn del primer (tenXCounter).
+  //Això demostra que els computed poden encadenar-se i Angular resoldrà les dependències automàticament.
+  hundredXCounter = computed(() => {
+    const val = this.tenXCounter();
+    return val * 10;
+  })
+
+  //Incrementa el counter en 1.
+  //Això farà que tenXCounter i hundredXCounter es recalculin automàticament gràcies al sistema reactiu de signals.
+  increment() {
+    this.counter.update(val => val + 1);
   }
 }
+
+
+//computed() és útil per valors derivats que es recalculen automàticament en funció d'altres signals.
