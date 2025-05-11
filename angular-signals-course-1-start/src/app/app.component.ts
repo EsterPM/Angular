3480cb6447c34a7f085d2115ee432f3d408ba678
Esther Pont <esther.cescit@gmail.com>
2025-05-11 12:22:08 +0200

    Authentication: Adapting

--- angular-signals-course-1-start/src/app/app.component.ts
+++ angular-signals-course-1-start/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './services/auth.service';
 import {Component, inject} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
@@ -19,6 +20,8 @@ import {MessagesComponent} from "./messages/messages.component";
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  authService = inject(AuthService);
 
+  isLoggedIn = this.authService.isLoggedIn;
 
 }
