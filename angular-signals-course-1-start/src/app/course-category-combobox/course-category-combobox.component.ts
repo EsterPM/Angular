c585666079ad59988c47da9b0869994b42cd55f4
Esther Pont <esther.cescit@gmail.com>
2025-05-11 13:11:41 +0200

    Model inputs

--- angular-signals-course-1-start/src/app/course-category-combobox/course-category-combobox.component.ts
+++ angular-signals-course-1-start/src/app/course-category-combobox/course-category-combobox.component.ts
@@ -1,5 +1,5 @@
-import {Component, input, model} from '@angular/core';
-import {CourseCategory} from "../models/course-category.model";
+import { Component, input, model } from '@angular/core';
+import { CourseCategory } from "../models/course-category.model";
 
 @Component({
   selector: 'course-category-combobox',
@@ -10,5 +10,14 @@ import {CourseCategory} from "../models/course-category.model";
 })
 export class CourseCategoryComboboxComponent {
 
+  //Declarem un input requerit de tipus string, que s'utilitza per mostrar l'etiqueta del <label> del combobox.
+  label = input.required<string>();
 
+  //Declarem un model requerit que representa la categoria seleccionada.
+  value = model.required<CourseCategory>();
+
+  //s'executa quan l'usuari canvia l'opció seleccionada del combobox.
+  onCategoryChanged(category: string) {
+    this.value.set(category as CourseCategory); //Converteix el valor (string) a CourseCategory i el desa en el model (value.set(...)).
+  }
 }
