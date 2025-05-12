197e6aed9062bea3fb4464fc877ac513d025333a
Esther Pont <esther.cescit@gmail.com>
2025-05-12 14:42:24 +0200

    Detail UI Pattern: contentChild

--- angular-signals-course-1-start/src/app/course-category-combobox/course-category-combobox.component.ts
+++ angular-signals-course-1-start/src/app/course-category-combobox/course-category-combobox.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, model } from '@angular/core';
+import { Component, contentChild, effect, ElementRef, input, model } from '@angular/core';
 import { CourseCategory } from "../models/course-category.model";
 
 @Component({
@@ -16,8 +16,22 @@ export class CourseCategoryComboboxComponent {
   //Declarem un model requerit que representa la categoria seleccionada.
   value = model.required<CourseCategory>();
 
+  //recuperant el div amb #title des de dins del component.
+  title = contentChild<ElementRef>("title");
+
+  constructor() {
+    effect(() => {
+      console.log("title: ", this.title());
+    });
+  }
+
   //s'executa quan l'usuari canvia l'opció seleccionada del combobox.
   onCategoryChanged(category: string) {
     this.value.set(category as CourseCategory); //Converteix el valor (string) a CourseCategory i el desa en el model (value.set(...)).
   }
 }
+
+
+//viewChild =	Elements que són dins la plantilla del component
+//contentChild = Elements projectats des de fora (via ng-content)
+//contentChildren = + d'un element
