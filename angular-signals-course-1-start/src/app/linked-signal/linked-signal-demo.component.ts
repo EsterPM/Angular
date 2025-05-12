d09ff4f6d3baf5411c6e29918fca4558ef479edb
Esther Pont <esther.cescit@gmail.com>
2025-05-12 18:26:26 +0200

    New signal Features: linkedSignal()

--- angular-signals-course-1-start/src/app/linked-signal/linked-signal-demo.component.ts
+++ angular-signals-course-1-start/src/app/linked-signal/linked-signal-demo.component.ts
@@ -27,7 +27,15 @@ export class LinkedSignalDemoComponent {
 
   selectedCourse = signal<string | null>("BEGINNERS");
 
-  quantity = signal(1);
+  //linkedSignal és un mecanisme que permet crear un signal que depèn d'altres signals, i actualitza el seu valor quan canvien els signals de dependència.
+  quantity = linkedSignal({
+    source: () => ({courseCode: this.selectedCourse}),
+    computation: (source, previous) => {
+      console.log(`linkedSignal source: `, source.courseCode());
+      console.log(`linkedSignal previous: `, previous);
+      return this.courses.find(c => c.code === source.courseCode())?.defaultQuantity ?? 1
+    }
+  });
 
   constructor() {
 
