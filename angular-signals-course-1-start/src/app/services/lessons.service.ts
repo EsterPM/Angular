1422208a910b45982aa9fe7e8b5b1572d171eb3b
Esther Pont <esther.cescit@gmail.com>
2025-05-12 10:45:38 +0200

    View Course page: Lessons Service

--- angular-signals-course-1-start/src/app/services/lessons.service.ts
+++ angular-signals-course-1-start/src/app/services/lessons.service.ts
@@ -1,9 +1,9 @@
-import {inject, Injectable} from "@angular/core";
-import {Lesson} from "../models/lesson.model";
+import { inject, Injectable } from "@angular/core";
+import { Lesson } from "../models/lesson.model";
 import { HttpClient, HttpParams } from "@angular/common/http";
-import {firstValueFrom} from "rxjs";
-import {GetLessonsResponse} from "../models/get-lessons.response";
-import {environment} from "../../environments/environment";
+import { firstValueFrom } from "rxjs";
+import { GetLessonsResponse } from "../models/get-lessons.response";
+import { environment } from "../../environments/environment";
 
 
 @Injectable({
@@ -11,6 +11,40 @@ import {environment} from "../../environments/environment";
 })
 export class LessonsService {
 
+  //configuració global de l'entorn (com l'URL de l'API)
   env = environment;
+
+  http = inject(HttpClient);
+
+  async loadLessons(config: {
+    courseId?: string,
+    query?: string;
+  }): Promise<Lesson[]> {
+
+    const { courseId, query } = config;
+
+    //l'objecte params amb els paràmetres que s'enviaran per URL
+    let params = new HttpParams();
+
+    //Si existeix, s'afegeix com a paràmetre.
+    if (courseId) {
+      params = params.set("courseId", courseId);
+    }
+    if (query) {
+      params = params.set("query", query);
+    }
+
+
+    const lessons$ = this.http.get<GetLessonsResponse>(
+      `${this.env.apiRoot}/search-lessons`,
+      {
+        params
+      }
+    )
+
+    //espera la resposta de l'observable i la transforma en una promesa.
+    const response = await firstValueFrom(lessons$);
+    return response.lessons;
+  }
 
 }
